f5f765cf5e7e1d110ad7a447edc884a733bcdb78
Josef Erben <josef@oxidizing.io>
2020-05-13 22:32:52 +0200

    Get rid of redundant type definition in module sig

--- sihl_email/lib/contract.ml
+++ sihl_email/lib/contract.ml
@@ -1,10 +1,8 @@
 module type REPOSITORY = sig
+  include Sihl_core.Contract.REPOSITORY
+
   val get :
     id:string ->
     Sihl_core.Db.connection ->
     Model.Template.t Sihl_core.Db.db_result
-
-  val migrate : unit -> Sihl_core.Contract.Migration.migration
-
-  val clean : Sihl_core.Db.connection -> unit Sihl_core.Db.db_result
 end
