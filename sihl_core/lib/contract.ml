0a69a2955b43d4e3edfa9b50e6556b77838c65fe
Josef Erben <josef@oxidizing.io>
2020-05-15 11:19:43 +0200

    Merge contract back to reduce overhead

--- sihl_core/lib/contract.ml
+++ sihl_core/lib/contract.ml
@@ -1,5 +1,57 @@
-module Email = Contract_email
-module Migration = Contract_migration
+module Email = struct
+  module type EMAIL = sig
+    type email
+
+    val send : Opium.Std.Request.t -> email -> (unit, string) result Lwt.t
+
+    (* val queue : Opium.Std.Request.t -> email -> (unit, string) result Lwt.t *)
+  end
+
+  module type REPOSITORY = sig
+    (* TODO *)
+  end
+end
+
+module Migration = struct
+  open Base
+
+  type migration_error = Caqti_error.t
+
+  type migration_operation =
+    Caqti_lwt.connection -> unit -> (unit, migration_error) Result.t Lwt.t
+
+  type migration_step = string * migration_operation
+
+  type migration = string * migration_step list
+
+  module type MIGRATION = sig
+    val migration : unit -> migration
+  end
+
+  module State = struct
+    type t = { namespace : string; version : int; dirty : bool }
+  end
+
+  module type REPOSITORY = sig
+    val create_table_if_not_exists :
+      (module Caqti_lwt.CONNECTION) ->
+      unit ->
+      (unit, [> Caqti_error.call_or_retrieve ]) Result.t Lwt.t
+
+    val get :
+      (module Caqti_lwt.CONNECTION) ->
+      namespace:string ->
+      (State.t option, [> Caqti_error.call_or_retrieve ]) Result.t Lwt.t
+
+    val upsert :
+      (module Caqti_lwt.CONNECTION) ->
+      State.t ->
+      (unit, [> Caqti_error.call_or_retrieve ]) Result.t Lwt.t
+  end
+
+  let repository : (module REPOSITORY) Registry.Key.t =
+    Registry.Key.create "migration repository"
+end
 
 module type REPOSITORY = sig
   val migrate : unit -> Migration.migration
