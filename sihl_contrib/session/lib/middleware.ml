7a8f4d4496602bb50cb5b60e943adcfdfe29138a
Josef Erben <josef@oxidizing.io>
2020-05-27 11:12:06 +0200

    Implement branch where cookie contains valid session

--- sihl_contrib/session/lib/middleware.ml
+++ sihl_contrib/session/lib/middleware.ml
@@ -18,27 +18,19 @@ let session () =
           Repository.get ~key:session_key |> Sihl.Core.Db.query_db_exn req
         in
         match session with
-        | Some _ ->
-            (* TODO
-               1. fetch session data
-               2. decode to (string * string) list
-               3. put into env
-            *)
-            handler req
+        | Some session ->
+            let env =
+              Opium.Hmap.add hmap_key session (Opium.Std.Request.env req)
+            in
+            handler { req with env }
         | None ->
-            (* TODO
-                       1. generate new session, store it in db
-                       2. send cookie with session id to override invalid cookie
-            *)
             let session = Model.Session.create () in
             let* () =
               Repository.insert session |> Sihl.Core.Db.query_db_exn req
             in
             let* res = handler req in
             res
-            |> Opium.Std.Cookie.set ~http_only:true ~secure:false
-                 ~key:cookie_key
-                 ~data:(Model.Session.key session)
+            |> Sihl.Http.Cookie.set ~key:cookie_key ~data:session.key
             |> Lwt.return )
     | None ->
         let session = Model.Session.create () in
@@ -50,29 +42,3 @@ let session () =
         |> Lwt.return
   in
   Opium.Std.Rock.Middleware.create ~name:"session" ~filter
-
-let set ~key ~value req =
-  let (module Repository : Repo.REPOSITORY) =
-    Sihl.Core.Registry.get Bind.Repository.key
-  in
-  let session =
-    match Opium.Hmap.find hmap_key (Opium.Std.Request.env req) with
-    | Some session -> session
-    | None ->
-        Sihl.Core.Err.raise_server
-          "Session not found in Request.env, have you applied the \
-           Sihl_session.middleware?"
-  in
-  let session = Model.Session.set ~key ~value session in
-  Repository.insert session |> Sihl.Core.Db.query_db_exn req
-
-let get key req =
-  let session =
-    match Opium.Hmap.find hmap_key (Opium.Std.Request.env req) with
-    | Some session -> session
-    | None ->
-        Sihl.Core.Err.raise_server
-          "Session not found in Request.env, have you applied the \
-           Sihl_session.middleware?"
-  in
-  Model.Session.get key session |> Lwt.return
--- sihl_contrib/session/lib/sihl_session.ml
+++ sihl_contrib/session/lib/sihl_session.ml
@@ -5,6 +5,28 @@ module Repo = Repo
 
 let middleware = Middleware.session
 
-let set = Middleware.set
+let set ~key ~value req =
+  let (module Repository : Repo.REPOSITORY) =
+    Sihl.Core.Registry.get Bind.Repository.key
+  in
+  let session =
+    match Opium.Hmap.find Middleware.hmap_key (Opium.Std.Request.env req) with
+    | Some session -> session
+    | None ->
+        Sihl.Core.Err.raise_server
+          "Session not found in Request.env, have you applied the \
+           Sihl_session.middleware?"
+  in
+  let session = Model.Session.set ~key ~value session in
+  Repository.insert session |> Sihl.Core.Db.query_db_exn req
 
-let get = Middleware.get
+let get key req =
+  let session =
+    match Opium.Hmap.find Middleware.hmap_key (Opium.Std.Request.env req) with
+    | Some session -> session
+    | None ->
+        Sihl.Core.Err.raise_server
+          "Session not found in Request.env, have you applied the \
+           Sihl_session.middleware?"
+  in
+  Model.Session.get key session |> Lwt.return
