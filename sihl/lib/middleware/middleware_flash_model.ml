c9ad12d7fcd2b745fb3998a037d09665ec2f8435
Josef Erben <josef@oxidizing.io>
2020-05-28 10:50:29 +0200

    Implement entry and message using ppx_deriving and inline tests

--- sihl/lib/middleware/middleware_flash_model.ml
+++ sihl/lib/middleware/middleware_flash_model.ml
@@ -2,26 +2,12 @@ open Base
 
 module Message = struct
   type t = Error of string | Warning of string | Success of string
-
-  let pp fmt (flash : t) =
-    let s =
-      match flash with
-      | Error msg -> msg
-      | Warning msg -> msg
-      | Success msg -> msg
-    in
-    Caml.Format.pp_print_string fmt s
-
-  let equal f1 f2 =
-    match (f1, f2) with
-    | Success msg1, Success msg2 -> String.equal msg1 msg2
-    | Warning msg1, Warning msg2 -> String.equal msg1 msg2
-    | Error msg1, Error msg2 -> String.equal msg1 msg2
-    | _ -> false
+  [@@deriving eq, show, yojson]
 end
 
 module Entry = struct
   type t = { current : Message.t option; next : Message.t option }
+  [@@deriving eq, show, yojson]
 
   let create message = { current = None; next = Some message }
 
@@ -30,4 +16,16 @@ module Entry = struct
   let next entry = entry.next
 
   let rotate entry = { current = entry.next; next = None }
+
+  let to_string entry = entry |> to_yojson |> Yojson.Safe.to_string
+
+  let of_string str = str |> Yojson.Safe.from_string |> of_yojson
 end
+
+(* Tests *)
+
+let%test "entry to and from string" =
+  let entry = Entry.create (Error "foo") in
+  Entry.equal
+    (entry |> Entry.to_string |> Entry.of_string |> Result.ok_or_failwith)
+    entry
--- sihl/lib/middleware/middleware_flash_model.mli
+++ sihl/lib/middleware/middleware_flash_model.mli
@@ -6,10 +6,18 @@ module Message : sig
   val pp : Caml.Format.formatter -> t -> unit
 
   val equal : t -> t -> bool
+
+  val to_yojson : t -> Yojson.Safe.t
+
+  val of_yojson : Yojson.Safe.t -> (t, string) Result.t
 end
 
 module Entry : sig
   type t = { current : Message.t option; next : Message.t option }
+
+  val pp : Caml.Format.formatter -> t -> unit
+
+  val equal : t -> t -> bool
 
   val create : Message.t -> t
 
@@ -18,4 +26,8 @@ module Entry : sig
   val next : t -> Message.t option
 
   val rotate : t -> t
+
+  val to_string : t -> string
+
+  val of_string : string -> (t, string) Result.t
 end
