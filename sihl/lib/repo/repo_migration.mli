a54e741945a3f48bc7dd064a89e141ebef64d6ae
Josef Erben <josef@oxidizing.io>
2020-06-08 12:57:58 +0200

    Add set_fk_check helper for MariaDB

--- sihl/lib/repo/repo_migration.mli
+++ sihl/lib/repo/repo_migration.mli
@@ -8,14 +8,15 @@ val execute :
   Core.Contract.Migration.migration list -> (unit, string) result Lwt.t
 
 module Mariadb : sig
+  val set_fk_check : Core.Db.db_connection -> bool -> unit Core.Db.db_result
+
   val migrate :
     ?disable_fk_check:bool ->
     string ->
-    (module Caqti_lwt.CONNECTION) ->
+    Core.Db.db_connection ->
     unit Core.Db.db_result
 end
 
 module Postgresql : sig
-  val migrate :
-    string -> (module Caqti_lwt.CONNECTION) -> unit Core.Db.db_result
+  val migrate : string -> Core.Db.db_connection -> unit Core.Db.db_result
 end
