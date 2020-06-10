e59125e96f62f495821aeceaa3221e199d635818
Josef Erben <josef@oxidizing.io>
2020-06-10 19:59:06 +0200

    Implement meta data for repository results

--- sihl/lib/repo/repo.ml
+++ sihl/lib/repo/repo.ml
@@ -1,1 +1,5 @@
+module Meta = struct
+  type t = { total : int } [@@deriving show, eq, fields, make]
+end
+
 module Migration = Repo_migration
--- sihl/lib/repo/repo.mli
+++ sihl/lib/repo/repo.mli
@@ -1,1 +1,15 @@
+module Meta : sig
+  type t
+
+  val equal : t -> t -> bool
+
+  val show : t -> string
+
+  val pp : Format.formatter -> t -> unit
+
+  val total : t -> int
+
+  val make : total:int -> t
+end
+
 module Migration = Repo_migration
