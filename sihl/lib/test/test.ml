5a0c9c90d207b0d3e2692a1e923ad97e9c11a5ef
Josef Erben <josef@oxidizing.io>
2020-06-23 15:32:43 +0200

    Throw exception in Test.seed, so we don't have to care when testing

--- sihl/lib/test/test.ml
+++ sihl/lib/test/test.ml
@@ -6,10 +6,6 @@ let request_with_connection () =
   "/mocked-request" |> Uri.of_string |> Cohttp_lwt.Request.make
   |> Opium.Std.Request.create |> Core.Db.request_with_connection
 
-let seed seed_fn =
-  let* request = request_with_connection () in
-  seed_fn request
-
 let register_services services =
   let config =
     Core.Config.Setting.create ~development:[]
@@ -21,3 +17,10 @@ let register_services services =
   let () = result |> Result.ok_or_failwith in
   let* result = Run.Project.Project.clean project in
   result |> Result.ok_or_failwith |> Lwt.return
+
+let seed seed_fn =
+  let* request = request_with_connection () in
+  let* result = seed_fn request in
+  match result with
+  | Ok result -> Lwt.return result
+  | Error msg -> failwith ("Failed to run seed " ^ msg)
--- sihl/lib/test/test.mli
+++ sihl/lib/test/test.mli
@@ -1,5 +1,5 @@
 val request_with_connection : unit -> Opium.Std.Request.t Lwt.t
 
-val seed : (Opium.Std.Request.t -> 'a Lwt.t) -> 'a Lwt.t
-
 val register_services : Core.Container.binding list -> unit Lwt.t
+
+val seed : (Opium.Std.Request.t -> ('a, string) Result.t Lwt.t) -> 'a Lwt.t
