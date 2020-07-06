9d369126791f93d2c2725091326d94f138ac5617
Josef Erben <josef@erben.io>
2020-07-06 13:09:14 +0200

    Reactive postgresql tests

---
+++ test/test-postgresql/test.ml
@@ -0,0 +1,21 @@
+open Alcotest_lwt
+
+let ( let* ) = Lwt.bind
+
+let suite = [ Test_common.Test.session ]
+
+let config =
+  Sihl.Config.create ~development:[]
+    ~test:[ ("DATABASE_URL", "postgres://admin:password@127.0.0.1:5432/dev") ]
+    ~production:[]
+
+let services =
+  [ Sihl.Data.Migration.Service.postgresql; Sihl.Session.Service.postgresql ]
+
+let () =
+  Lwt_main.run
+    (let* () =
+       let ctx = Sihl.Core.Ctx.empty in
+       Sihl.Test.app ctx ~config ~services
+     in
+     run "postgresql" @@ suite)
