325f535f54847e434c0cde9c0f2caf317b88dce2
Josef Erben <josef@erben.io>
2020-07-27 09:55:41 +0200

    Filter jobs to run in in-memory repo

--- src/queue/queue_service_repo.ml
+++ src/queue/queue_service_repo.ml
@@ -32,10 +32,11 @@ module MakeMemory (Repo : Data.Repo.Sig.SERVICE) : Queue_sig.REPO = struct
     let all_job_instances =
       List.map !ordered_ids ~f:(fun id -> Map.find !state id)
     in
+    let now = Ptime_clock.now () in
     let rec filter_pending all_job_instances result =
       match all_job_instances with
       | Some job_instance :: job_instances ->
-          if JobInstance.is_pending job_instance then
+          if JobInstance.should_run ~job_instance ~now then
             filter_pending job_instances (List.cons job_instance result)
           else filter_pending job_instances result
       | None :: job_instances -> filter_pending job_instances result
