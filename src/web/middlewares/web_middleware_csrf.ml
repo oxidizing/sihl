5a2578b0970e8974a8b38b0687db2442c27285f5
Josef Erben <josef@erben.io>
2020-08-05 13:26:41 +0200

    Remove warning for non-existing CSRF middleware

--- src/web/middlewares/web_middleware_csrf.ml
+++ src/web/middlewares/web_middleware_csrf.ml
@@ -2,6 +2,5 @@
 https://docs.djangoproject.com/en/3.0/ref/csrf/#how-it-works *)
 
 let m () =
-  Logs.warn (fun m -> m "WEB: CSRF middleware is not implemented");
   let filter handler ctx = handler ctx in
   Web_middleware_core.create ~name:"csrf" filter
