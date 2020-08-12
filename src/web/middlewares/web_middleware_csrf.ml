844e49be66fc75c7939463a6fda838056706be14
aronerben <aron.erben@gmail.com>
2020-08-12 23:15:09 +0200

    Add CSRF middleware
    To prevent CSRF, complete the CSRF middleware which checks if the
    tokens in requests match the ones in the database. This process can
    fail in multiple steps, thus the context is adjusted if it does.

--- src/web/middlewares/web_middleware_csrf.ml
+++ src/web/middlewares/web_middleware_csrf.ml
@@ -1,6 +1,39 @@
-(* TODO
-https://docs.djangoproject.com/en/3.0/ref/csrf/#how-it-works *)
+let ( let* ) = Lwt.bind
 
-let m () =
-  let filter handler ctx = handler ctx in
-  Web_middleware_core.create ~name:"csrf" filter
+module CsrfService = struct
+  let add_to_ctx ctx ~token () =
+    let open Core_ctx in
+    let key : Token.t key = create_key () in
+    add key token ctx
+end
+
+module Make
+    (MessageService : Message.Sig.Service)
+    (TokenService : Token.Sig.SERVICE) =
+struct
+  let m () =
+    let filter handler ctx =
+      let open Lwt_result.Infix in
+      let* res =
+        Web_req.urlencoded ctx "csrf" >>= fun value ->
+        TokenService.find ctx ~value ()
+      in
+      let _ =
+        match res with
+        | Ok token -> TokenService.invalidate ctx ~token ()
+        | Error _ ->
+            Logs.err (fun m -> m "MIDDLEWARE: Failed to retrieve CSRF token");
+            MessageService.set ctx ~error:[ "TODO ERROR MESSAGE" ] ()
+      in
+      let* token = TokenService.create ctx ~kind:"csrf" () in
+      let ctx =
+        match token with
+        | Ok token -> CsrfService.add_to_ctx ctx ~token ()
+        | Error msg ->
+            Logs.err (fun m -> m "MIDDLEWARE: Could not create token %s" msg);
+            ctx
+      in
+      handler ctx
+    in
+    Web_middleware_core.create ~name:"csrf" filter
+end
