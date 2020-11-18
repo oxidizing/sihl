03f08e7485c50b9da92670c084d3d5e23f7733e6
Josef Erben <josef@erben.io>
2020-11-18 18:29:14 +0100

    Simplify repository service implementation

--- sihl/repository/service.ml
+++ sihl/repository/service.ml
@@ -2,18 +2,18 @@ open Model
 open Lwt.Syntax
 module Sig = Sig
 
-module Registry = struct
-  let registry : cleaner list ref = ref []
-  let get_all () = !registry
-  let register cleaner = registry := List.cons cleaner !registry
-  let register_cleaners cleaners = registry := List.concat [ !registry; cleaners ]
-end
+let registered_cleaners : cleaner list ref = ref []
 
-let register_cleaner cleaner = Registry.register cleaner |> ignore
-let register_cleaners cleaners = Registry.register_cleaners cleaners |> ignore
+let register_cleaner cleaner =
+  registered_cleaners := List.cons cleaner !registered_cleaners
+;;
+
+let register_cleaners cleaners =
+  registered_cleaners := List.concat [ !registered_cleaners; cleaners ]
+;;
 
 let clean_all () =
-  let cleaners = Registry.get_all () in
+  let cleaners = !registered_cleaners in
   let rec clean_repos cleaners =
     match cleaners with
     | [] -> Lwt.return ()
