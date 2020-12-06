a01a6895032939fc7b1957bf99ab08b0309d2561
Josef Erben <josef@erben.io>
2020-12-06 10:06:06 +0100

    Get rid of unimplemented middlewares

--- sihl-web/src/middleware.ml
+++ sihl-web/src/middleware.ml
@@ -1,16 +1,9 @@
-module Clickjacking = Middleware_clickjacking
-module Cookie = Middleware_cookie
 module Csrf = Middleware_csrf
-module Error = Middleware_error
 module Flash = Middleware_flash
-module Gzip = Middleware_gzip
-module Security = Middleware_security
 module Static = Middleware_static
 module Session = Middleware_session
 module Authn = Middleware_authn
 module User = Middleware_user
 module Urlencoded = Middleware_urlencoded
 
-let error = Middleware_error.m
 let static = Middleware_static.m
-let cookie = Middleware_cookie.m
--- sihl-web/src/middleware_error.ml
+++
@@ -1,4 +0,0 @@
-let m () =
-  let filter handler req = handler req in
-  Opium_kernel.Rock.Middleware.create ~name:"error handler" ~filter
-;;
