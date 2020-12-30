01348fdb7707dae7910c98352e68f382c5eb2b81
Josef Erben <josef@oxidizing.io>
2020-12-30 23:23:27 +0100

    Hide internals of form middleware

--- sihl-web/src/form.mli
+++ sihl-web/src/form.mli
@@ -1,11 +1,7 @@
 type body = (string * string list) list
 
-val body_of_sexp : Ppx_sexp_conv_lib.Sexp.t -> body
-val sexp_of_body : body -> Ppx_sexp_conv_lib.Sexp.t
-
 exception Parsed_body_not_found
 
-val key : body Rock.Context.key
 val find_all : Rock.Request.t -> body
 val find : string -> Rock.Request.t -> string option
 val consume : Rock.Request.t -> string -> Rock.Request.t * string option
