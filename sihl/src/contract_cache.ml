8af7b606ed56ffaa6da115c83e011ecfa78ee08a
Josef Erben <josef@oxidizing.io>
2021-03-03 11:08:44 +0100

    Fix cache service API documentation
    Fix #406

--- sihl/src/contract_cache.ml
+++ sihl/src/contract_cache.ml
@@ -3,10 +3,11 @@ let name = "cache"
 exception Exception of string
 
 module type Sig = sig
-  (** [set value] inserts a [value] under a key. The value is a tuple where the
-      first element is the key and the second element is the value. Since key is
-      an optional, [set] can be used to remove value from the store like
-      [set "foo" None]. *)
+  (** [set entry] inserts an [entry] into the cache storage. [entry] is a tuple
+      where the first element is the key and the second element is the value.
+      Since the value is an optional, [set] can be used to remove a value from
+      the store like so: [set ("foo", None)]. If a key exists already, the value
+      is overwritten with the provided value. *)
   val set : string * string option -> unit Lwt.t
 
   (** [find key] returns the value that is associated with [key]. *)
