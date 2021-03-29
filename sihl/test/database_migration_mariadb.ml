90d039b029786f18a1b2278c6d1679bd58755589
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2021-03-29 08:34:16 +0200

    fix missing database url test configurations

--- sihl/test/database_migration_mariadb.ml
+++ sihl/test/database_migration_mariadb.ml
@@ -2,7 +2,9 @@ module Test = Database_migration.Make (Sihl.Database.Migration.MariaDb)
 
 let () =
   let open Lwt.Syntax in
-  Unix.putenv "DATABASE_URL" "mariadb://admin:password@127.0.0.1:3306/dev";
+  Sihl.Configuration.read_string "DATABASE_URL_TEST_MARIADB"
+  |> Option.value ~default:"mariadb://admin:password@127.0.0.1:3306/dev"
+  |> Unix.putenv "DATABASE_URL";
   Logs.set_level (Sihl.Log.get_log_level ());
   Logs.set_reporter (Sihl.Log.cli_reporter ());
   Lwt_main.run
