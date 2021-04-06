f195ecddd243a8f3385604939b11abb25a760cd6
Josef Erben <josef@oxidizing.io>
2021-04-06 16:45:22 +0200

    Add nix shell environment

---
+++ default.nix
@@ -0,0 +1,6 @@
+with import <nixpkgs> { };
+
+mkShell {
+  buildInputs = [ zlib.dev zlib.out zlib zlib.all gmp gmp.dev pkgconfig openssl libev libevdev mariadb-client mariadb-connector-c postgresql ];
+  LD_LIBRARY_PATH = "${mariadb-connector-c}/lib/mariadb";
+}
