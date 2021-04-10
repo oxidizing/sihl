73dd705de64127486df54a16917bb00bdf7cead7
Josef Erben <josef@oxidizing.io>
2021-04-10 09:02:03 +0200

    Eval opam env when running nix-shell

--- default.nix
+++ default.nix
@@ -3,4 +3,5 @@ with import <nixpkgs> { };
 mkShell {
   buildInputs = [ zlib.dev zlib.out zlib zlib.all gmp gmp.dev pkgconfig openssl libev libevdev mariadb-client mariadb-connector-c postgresql ];
   LD_LIBRARY_PATH = "${mariadb-connector-c}/lib/mariadb";
+  shellHook = "eval $(opam env)";
 }
