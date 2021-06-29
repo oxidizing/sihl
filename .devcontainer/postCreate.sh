c8ac43f4f47e82031a19b8695c3261700acf689b
Andi Saurer <andreas.saurer@econ.uzh.ch>
2021-06-29 08:36:44 +0200

    update devcontainer to the latest setup we use: - use opam image with ocaml 4.12 - add node and yarn - switch user instead of using sudo - add dependencies to dune-project instead of installing them manually

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,40 +1,16 @@
 # ocaml/opam post create script
 
-# VS code devContainer dependency
 sudo chown -R opam: _build
-
-# remove and update default ocaml remote
-# make sure that opam finds latest package versions
-# (e.g. otherwise alcotest latest version is 1.1.0 instead of 1.2.1)
-opam remote remove --all default
-opam remote add default https://opam.ocaml.org
-
-# Pins
-# opam pin add -yn ocaml-lsp-server https://github.com/ocaml/ocaml-lsp.git
-
-# install opam packages
-# e.g. when developing with emax, add also: utop merlin ocamlformat
-opam install \
-  alcotest-lwt \
-  caqti-driver-mariadb \
-  caqti-driver-postgresql \
-  caqti-lwt \
-  cohttp-lwt-unix \
-  letters \
-  ocaml-lsp-server.1.4.0 \
-  opium
-
-# install project dependancies
-# pin package
-opam pin add . --yes --no-action
-# Query and install external dependencies
-opam depext sihl sihl-user sihl-storage sihl-email sihl-queue sihl-cache sihl-token --yes --with-doc --with-test
-# install dependencies
-OPAMSOLVERTIMEOUT=180 opam install . --deps-only --with-doc --with-test --locked --unlock-base
-opam install ocamlformat --skip-updates
-# upgrade dependencies
-opam upgrade --fixup
+sudo chown -R opam: node_modules
 
 # initialize project and update environmemnt
-opam init
+opam init -a --shell=zsh
 eval $(opam env)
+
+# ensure all system dependencies are installed
+opam pin add . --yes --no-action
+opam depext sihl sihl-user sihl-storage sihl-email sihl-queue sihl-cache sihl-token --yes --with-doc --with-test
+
+# install opam packages used for vscode ocaml platform package
+# e.g. when developing with emax, add also: utop merlin ocamlformat
+make deps
