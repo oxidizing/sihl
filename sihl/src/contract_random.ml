de417b8dc4859ad1b48f06323956ee40d0d953e9
Josef Erben <josef@erben.io>
2021-10-22 17:41:24 +0200

    Add warning about generating random bytes

--- sihl/src/contract_random.ml
+++ sihl/src/contract_random.ml
@@ -1,12 +1,14 @@
 let name = "random"
 
 module type Sig = sig
-  (** [base64 n] returns a Base64 encoded string containing [n] random bytes. *)
+  (** [base64 n] returns a Base64 encoded string containing [n] random bytes.
+      This does not mean that the string contains [n] characters!. *)
   val base64 : int -> string
 
   (** [bytes n] returns a byte sequence as string with [n] random bytes. In most
       cases you want to use {!base64} to get a string that can be used safely in
-      most web contexts.*)
+      most web contexts. This does not mean that the string contains [n]
+      characters!.*)
   val bytes : int -> string
 
   val register : unit -> Core_container.Service.t
