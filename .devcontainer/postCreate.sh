0cdd3c00ec79c1f1bdac84a5e35ca9fbb466667d
Marc Biedermann <marc.biedermann@econ.uzh.ch>
2022-03-31 13:32:17 +0200

    update environment and devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -7,6 +7,10 @@ sudo chown -R opam: node_modules
 opam init -a --shell=zsh
 eval $(opam env)
 
+# get newest opam packages
+opam remote remove --all default
+opam remote add default https://opam.ocaml.org
+
 # ensure all system dependencies are installed
 opam pin add . --yes --no-action
 opam depext sihl sihl-user sihl-storage sihl-email sihl-queue sihl-cache sihl-token --yes --with-doc --with-test
@@ -14,3 +18,4 @@ opam depext sihl sihl-user sihl-storage sihl-email sihl-queue sihl-cache sihl-to
 # install opam packages used for vscode ocaml platform package
 # e.g. when developing with emax, add also: utop merlin ocamlformat
 make deps
+opam install -y ocaml-lsp-server
