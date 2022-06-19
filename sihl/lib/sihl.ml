9716de144b4bb6331dc39f1a166f13f5fd7a1727
Josef Erben <josef@erben.io>
2022-06-19 22:18:38 +0200

    Sort modules in sihl.ml

--- sihl/lib/sihl.ml
+++ sihl/lib/sihl.ml
@@ -1,11 +1,11 @@
 module Command = Sihl__command.Command
 module Config = Sihl__config.Config
-module Model = Sihl__model.Model
-module User = Sihl__user.User
 module Form = Sihl__form.Form
-module View = Sihl__view.View
-module Query = Sihl__query.Query
 module Migration = Sihl__migration.Migration
+module Model = Sihl__model.Model
+module Query = Sihl__query.Query
 module Test = Sihl__test.Test
+module User = Sihl__user.User
+module View = Sihl__view.View
 
 let if_debug m h = if Config.debug () then m h else h
